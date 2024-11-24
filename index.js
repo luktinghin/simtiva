ce0839381664006c80193d806707ae011f8f238a
Terence Luk <34789390+luktinghin@users.noreply.github.com>
2024-11-24 12:00:21 +0800

    .
    minor change - move sound back to main.js because it is declared as one of global variables

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-//section: sound and ringbell
-
-function sound(src) {
-  this.sound = document.createElement("audio");
-  this.sound.src = src;
-  this.sound.setAttribute("preload", "auto");
-  this.sound.setAttribute("controls", "none");
-  this.sound.style.display = "none";
-  document.body.appendChild(this.sound);
-  this.play = function(){
-    this.sound.play();
-  }
-  this.stop = function(){
-    this.sound.pause();
-  }
-}
+//section: ringbell
 
 
 function ringbell(duration) {
--- main.js
+++ main.js
@@ -4666,3 +4666,18 @@ function extendSession(ind) {
 		lookahead(0,21600,ind);
 	}
 }
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
